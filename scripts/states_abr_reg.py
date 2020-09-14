678a2e7266a986853ea0dbd680842f12d82969a3
Christopher Brown <christopherabrown42@gmail.com>
2020-09-13 20:02:13 -0400

    Create states_abr_reg.py
    Dictionary of US states, statecodes, and census regions

---
+++ scripts/states_abr_reg.py
@@ -0,0 +1,65 @@
+# Modified version of Roger Allen's code from:
+# https://gist.github.com/rogerallen/1583593
+#
+# U.S. Region names added by Christopher Brown, 9/13/20
+# Northeast, Midwest, South, West defintions:
+# https://www2.census.gov/geo/pdfs/maps-data/maps/reference/us_regdiv.pdf
+
+us_st_abr_reg = {
+    'Alabama': ['AL', 'South'],
+    'Alaska': ['AK', 'West'],
+    'American Samoa': ['AS', 'Other'], #
+    'Arizona': ['AZ', 'West'],
+    'Arkansas': ['AR', 'South'],
+    'California': ['CA', 'West'],
+    'Colorado': ['CO', 'West'],
+    'Connecticut': ['CT', 'Northeast'],
+    'Delaware': ['DE', 'South'],
+    'District of Columbia': ['DC', 'South'],
+    'Florida': ['FL', 'South'],
+    'Georgia': ['GA', 'South'],
+    'Guam': ['GU', 'Other'], #
+    'Hawaii': ['HI', 'West'],
+    'Idaho': ['ID', 'West'],
+    'Illinois': ['IL', 'Midwest'],
+    'Indiana': ['IN', 'Midwest'],
+    'Iowa': ['IA', 'Midwest'],
+    'Kansas': ['KS', 'Midwest'],
+    'Kentucky': ['KY', 'South'],
+    'Louisiana': ['LA', 'South'],
+    'Maine': ['ME', 'Northeast'],
+    'Maryland': ['MD', 'South'],
+    'Massachusetts': ['MA', 'Northeast'],
+    'Michigan': ['MI', 'Midwest'],
+    'Minnesota': ['MN', 'Midwest'],
+    'Mississippi': ['MS', 'South'],
+    'Missouri': ['MO', 'Midwest'],
+    'Montana': ['MT', 'West'],
+    'Nebraska': ['NE', 'Midwest'],
+    'Nevada': ['NV', 'West'],
+    'New Hampshire': ['NH', 'Northeast'],
+    'New Jersey': ['NJ', 'Northeast'],
+    'New Mexico': ['NM', 'West'],
+    'New York': ['NY', 'Northeast'],
+    'North Carolina': ['NC', 'South'],
+    'North Dakota': ['ND', 'Midwest'],
+    'Northern Mariana Islands': ['MP', 'Other'], #
+    'Ohio': ['OH', 'Midwest'],
+    'Oklahoma': ['OK', 'South'],
+    'Oregon': ['OR', 'West'],
+    'Pennsylvania': ['PA', 'Northeast'],
+    'Puerto Rico': ['PR', 'Other'], #
+    'Rhode Island': ['RI', 'Northeast'],
+    'South Carolina': ['SC', 'South'],
+    'South Dakota': ['SD', 'Midwest'],
+    'Tennessee': ['TN', 'South'],
+    'Texas': ['TX', 'South'],
+    'Utah': ['UT', 'West'],
+    'Vermont': ['VT', 'Northeast'],
+    'Virgin Islands': ['VI', 'Other'],  #
+    'Virginia': ['VA', 'South'],
+    'Washington': ['WA', 'West'],
+    'West Virginia': ['WV', 'South'],
+    'Wisconsin': ['WI', 'Midwest'],
+    'Wyoming': ['WY', 'West']
+}
